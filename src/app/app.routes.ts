3f309f2bdcb39a8fa76d2b3d53f4bbb2ac95cd60
Anuragmishra880 <anuragmishra9821497411@gmail.com>
2026-07-26 19:23:32 +0530

    Design Navbar footer and hero section

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
+import { Hero } from './Features/Modules/Pages/hero/hero';
+import { Footer } from './Shared/Component/footer/footer';
+import { Navbar } from './Shared/Component/navbar/navbar';
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+
+
+export const routes: Routes = [
+
+  {
+     path:"", component:Hero
+    },
+  
+
+];
